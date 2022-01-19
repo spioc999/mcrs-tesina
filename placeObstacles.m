bd2eaf0d73784c655ecf91db50d7527087d3cffa
Simone Pio Caronia <spcaronia@gmail.com>
2022-01-19 19:56:05 +0100

    inserted config txt

--- placeObstacles.m
+++ placeObstacles.m
@@ -2,6 +2,7 @@ function [] = placeObstacles(rectangles)
     rect = findall(gcf,'Type', 'Rectangle'); 
     delete(rect)
     rectangle('Position',[0 0 10 10]); % Field
+    rectangle('Position',[4 4 2 2], 'Curvature',1, 'FaceColor','#4487d4'); %% Blocked area
 
     rectangle('Position',[0.5 2 1 2]); 
     rectangle('Position',[1 8 3 1]);
@@ -10,5 +11,4 @@ function [] = placeObstacles(rectangles)
     rectangle('Position',[1 5 1.5 1.5]);
     rectangle('Position',[2 1 4 2]);
     rectangle('Position',[7.5 4.5 2 3]);
-    rectangle('Position',[4 4 2 2], 'Curvature',1, 'FaceColor','#4487d4'); %% Blocked area
 end
