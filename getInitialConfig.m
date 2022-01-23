f5d310d7c266f82a1839781a0472e59b5fb2081f
Paolo Ruggirello <pruggirello@soldo.com>
2022-01-23 01:58:15 +0100

    WIP

--- getInitialConfig.m
+++ getInitialConfig.m
@@ -1,30 +1,57 @@
 function [rectanglesPosition, rectanglesMovementEnabled, rectanglesDirection, rectanglesDelta] = getInitialConfig(filePath)
-    FID = fopen(filePath);
-    skipFileHeader(FID)
-    rectanglesConfig = fscanf(FID, '%f %f %f %f %d %d %f');
-    rectanglesPosition = getRectanglesPosition(rectanglesConfig);
-    rectanglesMovementEnabled = rectanglesConfig(5:7:end);
-    rectanglesDirection = rectanglesConfig(6:7:end);
-    rectanglesDelta = rectanglesConfig(7:7:end);
+    rectanglesPosition = [];
+    rectanglesMovementEnabled = [];
+    rectanglesDirection = []; 
+    rectanglesDelta = [];
+
+    fileLines = skipHeader(readlines(filePath));
+    for lineNumber=1:size(fileLines, 1)
+        rectanglesPosition = cat(1, rectanglesPosition, getRectanglesPosition(fileLines(lineNumber, 1:4)));
+        rectanglesMovementEnabled = cat(1, rectanglesMovementEnabled, getRectangleMovementEnabled(fileLines(lineNumber, 5)));
+
+        
+        
+        rectanglesDirection = rectanglesConfig(6:7:end);
+        rectanglesDelta = rectanglesConfig(7:7:end);
+    end
+    
 end
 
 
-function [] = skipFileHeader(FID)
-    readedLine = fgetl(FID);
-    firstChar = readedLine(1);
-    while firstChar == '#'
-        readedLine = fgetl(FID);
-        if strlength(readedLine) > 0
-            firstChar = readedLine(1);
-        else
-            firstChar = readedLine;
+function [fixedFileLines] = skipHeader(fileLines)
+    fixedFileLines = [];
+    for lineNumber=1:size(fileLines, 1)
+        lineElements = split(fileLines(lineNumber));
+        if not(strcmp(lineElements(1), "")) && not(strcmp(lineElements(1), '#'))
+            fixedFileLines = cat(1, fixedFileLines, lineElements');
         end
     end
-    if not(strcmp(readedLine, ''))
-        error('Config file wrong format. You must leave an empty line before the configuration.')
+end
+
+function [rectanglesPosition] = getRectanglesPosition(lineData)
+    x = getValueFromStringOrRandomInRange(lineData(1), 0, 9, 'float');
+    y = getValueFromStringOrRandomInRange(lineData(2), 0, 9, 'float');
+    w = getValueFromStringOrRandomInRange(lineData(3), 1, min([4, 10-x]), 'float'); 
+    h = getValueFromStringOrRandomInRange(lineData(4), 1, min(4, 10-y), 'float');
+    rectanglesPosition = [x y w h];
+end
+
+function [rectangleMovementEnabled] = getRectangleMovementEnabled(movementEnabled)
+    rectangleMovementEnabled = getValueFromStringOrRandomInRange(movementEnabled, 0, 1, 'int');
+    if rectangleMovementEnabled > 1 || rectangleMovementEnabled < 0
+        error("MovementEnabled, in config file, must be 0 or 1.")
     end
 end
 
-function [rectanglesPosition] = getRectanglesPosition(rectanglesConfig)
-    rectanglesPosition = [rectanglesConfig(1:7:end), rectanglesConfig(2:7:end), rectanglesConfig(3:7:end), rectanglesConfig(4:7:end)];
+function [value] = getValueFromStringOrRandomInRange(strValue, rangeInit, rangeEnd, randType)
+    if strcmp(strValue, '*')
+        if not(strcmp(randType, 'int'))
+            value = (rangeEnd-rangeInit).*rand + rangeInit;
+        else
+            value = randi([rangeInit, rangeEnd]);
+        end
+    else
+        value = str2double(strValue);
+    end
 end
+
