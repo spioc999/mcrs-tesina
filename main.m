45e22572ba0c2437c550b8e18e10bded67dfbb7a
Paolo Ruggirello <pruggirello@soldo.com>
2022-01-25 23:28:06 +0100

    restored main

--- main.m
+++ main.m
@@ -24,30 +24,19 @@ currentPosition = [0 0];
 destination = [10 10];
 
 
-% donePath = animatedline('Color','b','LineWidth',3);
-% toDotPath = animatedline('Color','r','LineWidth',3);
+donePath = animatedline('Color','b','LineWidth',3);
+toDotPath = animatedline('Color','r','LineWidth',3);
 
 while not(currentPosition == destination)
-%     pathToDestination = findPath(currentPosition, destination, rectanglesPosition);
-%     clearpoints(toDotPath);
-%     addpoints(toDotPath, pathToDestination(:, 1), pathToDestination(:,2));
-%     addpoints(donePath, currentPosition(1), currentPosition(2));
-%     drawnow;
+    pathToDestination = findPath(currentPosition, destination, rectanglesPosition);
+    clearpoints(toDotPath);
+    addpoints(toDotPath, pathToDestination(:, 1), pathToDestination(:,2));
+    addpoints(donePath, currentPosition(1), currentPosition(2));
+    drawnow;
     placeObstacles(rectanglesPosition);
     
     [rectanglesPosition, rectanglesDirection, rectanglesDelta] = calculateNewObstaclesPosition(rectanglesPosition, rectanglesMovementEnabled, rectanglesDirection, rectanglesDelta);
-%     currentPosition = pathToDestination(2, :);
+    currentPosition = pathToDestination(2, :);
     pause(0.1);
 end
 
-
-
-
-% while true
-%     placeObstacles(rectanglesPosition);
-%     [positions, directions, deltas] = calculateNewObstaclesPosition(rectanglesPosition, rectanglesMovementEnabled, rectanglesDirection, rectanglesDelta);
-%     rectanglesPosition = positions;
-%     rectanglesDirection = directions;
-%     rectanglesDelta = deltas;
-%     pause(0.05);
-% end
